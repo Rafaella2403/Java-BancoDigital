f75c178aa1a44ef0961b9b1f829e1441bd48700f
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-01-23 00:30:06 -0300

    Implemetando a classe Cliente

--- src/entidades/Cliente.java
+++ src/entidades/Cliente.java
@@ -2,6 +2,31 @@ package entidades;
 
 public class Cliente {
 
+	private String nomeCliente;
+	private int cpfCliente;
+	private int agencia;
 	
+	public Cliente(String nomeCliente, int cpfCliente, int agencia) {
+		super();
+		this.nomeCliente = nomeCliente;
+		this.cpfCliente = cpfCliente;
+		this.agencia = agencia;
+	}
+	
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public int getAgencia() {
+		return agencia;
+	}
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+	public int getCpfCliente() {
+		return cpfCliente;
+	}
 
 }
